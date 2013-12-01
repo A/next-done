fae52b5f6692544d9092824664be1cedf915303e
Shuvalov Anton <anton_shuvalov@me.com>
2013-12-01 17:15:39 +0600

    Стилистическая правка. Удалил node_debug.log

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = function (count, done) {
   if (!done) throw new Error('`done` must be a function');
   return function (err) {
     err
-      ? done (err)
+      ? done(err)
       : --count
         ? null
         : done();
