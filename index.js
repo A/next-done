88b99a2048137962f9fb93fb778dc2b99fe8bac3
Shuvalov Anton <anton_shuvalov@me.com>
2013-12-03 15:55:33 +0600

    Update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (count, done) {
-  if (!('function' === typeof done)) throw new Error('`done` must be a function');
+  if ('function' !== typeof done) throw new Error('`done` must be a function');
   return function (err) {
     (!--count || err)
       ? done(err)
