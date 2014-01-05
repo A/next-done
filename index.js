514b025cda50a79bc97fcaaa7ee2a400dad0e8e2
Shuvalov Anton <anton_shuvalov@me.com>
2014-01-05 18:43:49 +0600

    close #4, close #1. v0.2.0

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 'use strict';
 
-module.exports = function (count, done) {
-  if ('function' !== typeof done) throw new Error('`done` must be a function');
+module.exports = function (count, cb) {
+
+  var done = (function () {
+    var isDone = false;
+    return function (err) {
+      if (isDone) {
+        throw new Error ('`done` called multiple times.');
+      } else {
+        isDone = true;
+        cb(err);
+      }
+    }
+  })();
+
+
   return function (err) {
     (!--count || err)
       ? done(err)
-      : null;
+      : void 0;
   };
+
 };
--- test.js
+++ test.js
@@ -8,19 +8,44 @@
 
 var Countdown      = require('./index');
 var assert         = require('assert');
+var should         = require('should');
 
 // End of dependencies.
 
+
 describe('test', function(){
 
-  /**
-   * No route
-   */
-  it('next 3 times', function(done){
+  it('should next 3 times, then done', function(done){
     var next = new Countdown(3, done);
     next();
     next();
     next();
+  });
+
+
+  it('should return error', function(done){
+    var next = new Countdown(1, function (err) {
+      should.exist(err);
+      done();
+    });
+    next(new Error('Test if error'));
+  });
+
+
+  it('should return error and other next() calls must throw errors', function(done){
+    var next = new Countdown(2, function (err) {
+      // called only once
+      should.exist(err);
+    });
+ 
+    next(new Error('Just some error'));
+
+    try {
+      next(); // throw error
+    } catch (err) {
+      assert(err.toString() === 'Error: `done` called multiple times.');
+      done()
+    }
   });
 
 });
